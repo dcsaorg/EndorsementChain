2604dd80e590dcc3389cf8b14fdfc65c19795f00
Nicolas Guilbert <nicolas@ange.dk>
2022-03-08 14:29:43 +0100

    Check validity of private key upon file upload

--- static/loadprivatekey.js
+++ static/loadprivatekey.js
@@ -1,13 +1,23 @@
 /*
  * A function to load a text (a private key) into a text area
+ * Optionally checks that the private corresponds to a provided public key (JWK)
  */
 
-function loadPrivateKeyFromFile(loadKey, keyTextArea){
+function loadPrivateKeyFromFile(loadKey, keyTextArea, publicKeyJWK, checkMarkDivId){
     const loadChosenFile = document.getElementById(loadKey).files[0];
     const privateKeyfileReader = new FileReader();
     privateKeyfileReader.onload = function(fileLoadedEvent) {
         const textFromPrivateKey = fileLoadedEvent.target.result;
         document.getElementById(keyTextArea).value = textFromPrivateKey;
+        if (publicKeyJWK) {
+            document.getElementById(checkMarkDivId).innerHTML = "❌";
+            let challengeDummyBlock = new TransferBlock();
+            challengeDummyBlock.init(null, null, textFromPrivateKey);
+            const isValid = challengeDummyBlock.verifyNth(0, KEYUTIL.getKey(publicKeyJWK), {alg: ['RS256']});
+            if (isValid) {
+                document.getElementById(checkMarkDivId).innerHTML = "✅";
+            }
+        }
     };
     privateKeyfileReader.readAsText(loadChosenFile, "UTF-8");
 }
