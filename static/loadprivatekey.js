e71ce2f6791f42069269e59182430d4f144878ce
Sacha Guilbert <sachaguilbert@gmail.com>
2022-03-18 18:43:16 -0400

    Add checks to import URL and private key

--- static/loadprivatekey.js
+++ static/loadprivatekey.js
@@ -3,20 +3,17 @@
  * Optionally checks that the private corresponds to a provided public key (JWK)
  */
 
-function loadPrivateKeyFromFile(loadKey, keyTextArea, publicKeyJWK, checkMarkDivId){
+function loadPrivateKeyFromFile(loadKey, keyTextArea, publicKeyJWK, whenValidDo){
     const loadChosenFile = document.getElementById(loadKey).files[0];
     const privateKeyfileReader = new FileReader();
     privateKeyfileReader.onload = function(fileLoadedEvent) {
         const textFromPrivateKey = fileLoadedEvent.target.result;
         document.getElementById(keyTextArea).value = textFromPrivateKey;
         if (publicKeyJWK) {
-            document.getElementById(checkMarkDivId).innerHTML = "❌";
             let challengeDummyBlock = new TransferBlock();
             challengeDummyBlock.init(null, null, textFromPrivateKey);
             const isValid = challengeDummyBlock.verifyNth(0, KEYUTIL.getKey(publicKeyJWK), {alg: ['RS256']});
-            if (isValid) {
-                document.getElementById(checkMarkDivId).innerHTML = "✅";
-            }
+            whenValidDo(isValid);
         }
     };
     privateKeyfileReader.readAsText(loadChosenFile, "UTF-8");
