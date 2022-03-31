c17da56bf4cebdd756a29782fb147a0f28c00c61
Nicolas Guilbert <nicolas@ange.dk>
2022-03-31 15:15:27 +0200

    Test JWK endpoint without starting the application

--- src/test/java/dk/ange/jwtexperiment/JwkSetEndPointTest.java
+++ src/test/java/dk/ange/jwtexperiment/JwkSetEndPointTest.java
@@ -1,24 +1,24 @@
 package dk.ange.jwtexperiment;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.http.MediaType;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest(classes = Application.class)
-@WebAppConfiguration
-@AutoConfigureMockMvc
+@WebMvcTest(JwkSetRestController.class)
+@ContextConfiguration(classes = {JwkBeanFactory.class, SecurityConfig.class, JwkSetRestController.class})
 public class JwkSetEndPointTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private JwkSetRestController jwkSetRestController;
 
     @Test
     public void getJwks() throws Exception {
