e81a002883d37e71e2b9e1f1c85a5341ce89be1c
Nicolas Guilbert <nicolas@ange.dk>
2022-04-07 14:37:25 +0200

    Use standard port for essDOCS

--- src/main/resources/static/testplatforms.js
+++ src/main/resources/static/testplatforms.js
@@ -4,7 +4,7 @@
 
 var testPlatforms = [
     {name: "edoxOnline", url: "dcsapoc.edoxonline.com:8443"},
-    {name: "essDOCS", url: "dcsa.cargodocs.com:80"},
+    {name: "essDOCS", url: "dcsa.cargodocs.com"},
     {name: "CargoX", url: "dcsa2.cargox.cc"},
     {name: "DCSA Test Platform 1", url: "01.dcsa.ange.dk:8443"},
     {name: "DCSA Test Platform 2", url: "02.dcsa.ange.dk:8443"}
