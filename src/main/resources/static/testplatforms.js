69acf35de8119742ed69f57428a6c64a0e92e3d9
Nicolas Gulibert <nicolas@ange.dk>
2022-04-13 11:55:09 +0200

    Update testplatforms.js with Wave BL

--- src/main/resources/static/testplatforms.js
+++ src/main/resources/static/testplatforms.js
@@ -3,9 +3,10 @@
  */
 
 var testPlatforms = [
+    {name: "Wave BL", url: "dcsa-poc.dev1.dev.wavebl.com:8443"},
     {name: "edoxOnline", url: "dcsapoc.edoxonline.com:8443"},
     {name: "essDOCS", url: "dcsa.cargodocs.com"},
     {name: "CargoX", url: "dcsa2.cargox.cc"},
-    {name: "DCSA Test Platform 1", url: "01.dcsa.ange.dk:8443"},
-    {name: "DCSA Test Platform 2", url: "02.dcsa.ange.dk:8443"}
+    {name: "DCSA Test 1", url: "01.dcsa.ange.dk:8443"},
+    {name: "DCSA Test 2", url: "02.dcsa.ange.dk:8443"}
 ];
