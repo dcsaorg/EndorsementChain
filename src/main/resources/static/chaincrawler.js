60c984f7645e382a294e1f08a442052c2929c9ad
Nicolas Guilbert <nicolas@ange.dk>
2022-07-18 14:27:35 +0200

    Fix indentation to 2 spaces

--- src/main/resources/static/chaincrawler.js
+++ src/main/resources/static/chaincrawler.js
@@ -3,91 +3,91 @@
  */
 
 class ChainCrawler {
-    async crawlPossessionChain(initPossessionTdtUrl) {
-        let possessionChain = [];
-        let platformChain = [];
-        let titleHolderChain = [];
-        let possessorPlatformChain = [];
-        let titleHolderPlatformChain = [];
-        const url = new URL(initPossessionTdtUrl);
-        let serverName = url.host;
-        const apiPath = (url.pathname).match(/.*\//)[0];
-        let currentPossessionUrl = initPossessionTdtUrl;
-        let possessionBlock;
-        do {
-            const currentTdt= await (await fetch(currentPossessionUrl)).json();
-            platformChain.push(serverName);
-            possessionBlock = (new PossessionTransferBlock(currentTdt.transferBlock));
-            possessionChain.push(possessionBlock);
-            const previousRegistryURL = possessionBlock.blockPayloadAsJson()["previousRegistryURL"]; //note: only non-null if block is an import block
-            if (previousRegistryURL) {
-                serverName = (new URL(previousRegistryURL).host);
-            }
-            currentPossessionUrl = "https://" + serverName + apiPath + possessionBlock.previousBlockHash();
-            let currentTitleUrl = "https://" + serverName + apiPath + possessionBlock.titleTransferBlockHash();
-            let currentTitleTdt= await (await fetch(currentTitleUrl)).json();
-            const currentTitleTransferBlock = new TitleTransferBlock(currentTitleTdt.transferBlock);
-            titleHolderChain.push(currentTitleTransferBlock);
-            titleHolderPlatformChain.push(currentTitleTransferBlock.titleHolderPlatform());
-        } while (possessionBlock.previousBlockHash() != null);
+  async crawlPossessionChain(initPossessionTdtUrl) {
+    let possessionChain = [];
+    let platformChain = [];
+    let titleHolderChain = [];
+    let possessorPlatformChain = [];
+    let titleHolderPlatformChain = [];
+    const url = new URL(initPossessionTdtUrl);
+    let serverName = url.host;
+    const apiPath = (url.pathname).match(/.*\//)[0];
+    let currentPossessionUrl = initPossessionTdtUrl;
+    let possessionBlock;
+    do {
+      const currentTdt= await (await fetch(currentPossessionUrl)).json();
+      platformChain.push(serverName);
+      possessionBlock = (new PossessionTransferBlock(currentTdt.transferBlock));
+      possessionChain.push(possessionBlock);
+      const previousRegistryURL = possessionBlock.blockPayloadAsJson()["previousRegistryURL"]; //note: only non-null if block is an import block
+      if (previousRegistryURL) {
+          serverName = (new URL(previousRegistryURL).host);
+      }
+      currentPossessionUrl = "https://" + serverName + apiPath + possessionBlock.previousBlockHash();
+      let currentTitleUrl = "https://" + serverName + apiPath + possessionBlock.titleTransferBlockHash();
+      let currentTitleTdt= await (await fetch(currentTitleUrl)).json();
+      const currentTitleTransferBlock = new TitleTransferBlock(currentTitleTdt.transferBlock);
+      titleHolderChain.push(currentTitleTransferBlock);
+      titleHolderPlatformChain.push(currentTitleTransferBlock.titleHolderPlatform());
+    } while (possessionBlock.previousBlockHash() != null);
 
-        const nbBlocks = platformChain.length;
-        let currentTitleHolder = titleHolderChain[nbBlocks-1]; //first title holder and platform (last items of their resp. arrays)
-        let currentTitlePlatform = platformChain[nbBlocks-1];
-        let titlePlatformChain = [currentTitlePlatform];
-        const initialPossessorPlatformPossiblyExport = possessionChain[nbBlocks - 1].blockPayloadAsJson()["nextRegistryHost"];
-        let currentPossessorPlatform = initialPossessorPlatformPossiblyExport ? initialPossessorPlatformPossiblyExport : platformChain[nbBlocks - 1];
-        possessorPlatformChain = [currentPossessorPlatform];
-        for (let i = nbBlocks-2; i >=0; --i) {
-            if(currentTitleHolder != titleHolderChain[i]) {
-                currentTitleHolder = titleHolderChain[i];
-                currentTitlePlatform = platformChain[i];
-            }
-            titlePlatformChain.splice(0, 0, currentTitlePlatform);
-            const nextRegistryHost = possessionChain[i].blockPayloadAsJson()["nextRegistryHost"]; //note: only non-null if block is an export block
-            if (nextRegistryHost) {
-                currentPossessorPlatform = nextRegistryHost;
-            }
-            possessorPlatformChain.splice(0, 0, currentPossessorPlatform);
-        }
-
-        let statuses = new Array(possessionChain.length);
-        statuses[statuses.length-1] = "ISSU";
-        for (let i = 1; i < statuses.length-1; ++i) {
-          if(possessionChain[i].titleTransferBlockHash() != possessionChain[i-1].titleTransferBlockHash()) {
-            statuses[i] = "ENOR";
-          } else {
-            statuses[i] = "POSS";
-          }
-        }
-        let latestTransfereeJWK = possessionChain[0].transferee();
-        if(possessionChain[statuses.length-1].verifyNth(0, KEYUTIL.getKey(latestTransfereeJWK), {alg: ['RS256']})) {
-          statuses[0] = "SURR";
-        } else {
-          statuses[0] = "POSS";
-        }
-
-        return {"possessionChain": possessionChain, "platformChain": platformChain,
-                "titleHolderChain": titleHolderChain, "titlePlatformChain": titlePlatformChain,
-                "possessorPlatformChain": possessorPlatformChain, "titleHolderPlatformChain": titleHolderPlatformChain,
-                "statuses": statuses}
+    const nbBlocks = platformChain.length;
+    let currentTitleHolder = titleHolderChain[nbBlocks-1]; //first title holder and platform (last items of their resp. arrays)
+    let currentTitlePlatform = platformChain[nbBlocks-1];
+    let titlePlatformChain = [currentTitlePlatform];
+    const initialPossessorPlatformPossiblyExport = possessionChain[nbBlocks - 1].blockPayloadAsJson()["nextRegistryHost"];
+    let currentPossessorPlatform = initialPossessorPlatformPossiblyExport ? initialPossessorPlatformPossiblyExport : platformChain[nbBlocks - 1];
+    possessorPlatformChain = [currentPossessorPlatform];
+    for (let i = nbBlocks-2; i >=0; --i) {
+      if(currentTitleHolder != titleHolderChain[i]) {
+        currentTitleHolder = titleHolderChain[i];
+        currentTitlePlatform = platformChain[i];
+      }
+      titlePlatformChain.splice(0, 0, currentTitlePlatform);
+      const nextRegistryHost = possessionChain[i].blockPayloadAsJson()["nextRegistryHost"]; //note: only non-null if block is an export block
+      if (nextRegistryHost) {
+          currentPossessorPlatform = nextRegistryHost;
+      }
+      possessorPlatformChain.splice(0, 0, currentPossessorPlatform);
     }
 
-    async chainToThumbprints(promiseChain) {
-        const awaitedChain = await Promise.all(
-            promiseChain.map(function(transferBlock) {
-                return transferBlock.transfereeThumbprint();
-            })
-        );
-        return awaitedChain;
+    let statuses = new Array(possessionChain.length);
+    statuses[statuses.length-1] = "ISSU";
+    for (let i = 1; i < statuses.length-1; ++i) {
+      if(possessionChain[i].titleTransferBlockHash() != possessionChain[i-1].titleTransferBlockHash()) {
+        statuses[i] = "ENOR";
+      } else {
+        statuses[i] = "POSS";
+      }
+    }
+    let latestTransfereeJWK = possessionChain[0].transferee();
+    if(possessionChain[statuses.length-1].verifyNth(0, KEYUTIL.getKey(latestTransfereeJWK), {alg: ['RS256']})) {
+      statuses[0] = "SURR";
+    } else {
+      statuses[0] = "POSS";
     }
 
-    async chainToTimestamps(promiseChain) {
-        const awaitedChain = await Promise.all(
-            promiseChain.map(function(transferBlock) {
-                return transferBlock.timestamp();
-            })
-        );
-        return awaitedChain;
-    }
+    return {"possessionChain": possessionChain, "platformChain": platformChain,
+            "titleHolderChain": titleHolderChain, "titlePlatformChain": titlePlatformChain,
+            "possessorPlatformChain": possessorPlatformChain, "titleHolderPlatformChain": titleHolderPlatformChain,
+            "statuses": statuses}
+  }
+
+  async chainToThumbprints(promiseChain) {
+    const awaitedChain = await Promise.all(
+      promiseChain.map(function(transferBlock) {
+        return transferBlock.transfereeThumbprint();
+      })
+    );
+    return awaitedChain;
+  }
+
+  async chainToTimestamps(promiseChain) {
+    const awaitedChain = await Promise.all(
+      promiseChain.map(function(transferBlock) {
+        return transferBlock.timestamp();
+      })
+    );
+    return awaitedChain;
+  }
 }
--- src/main/resources/static/spec/chaincrawlerSpec.js
+++ src/main/resources/static/spec/chaincrawlerSpec.js
@@ -1,21 +1,21 @@
 describe("Chaincrawler", function() {
-    it("should properly convert a possession chain to thumbprints", async function() {
-        let chainCrawler = new ChainCrawler();
-        let possessionChain = [ new PossessionTransferBlock(
-            {"payload":"eyJ0cmFuc2ZlcmVlIjp7Imt0eSI6IlJTQSIsIm4iOiJ4cEZzaXhpZHgxMnUzc0I2WnBRWW44SWN1SVlqQmZHTTBSbHUzVG9vWkhPa0E1SGJwdlJ2c2FqX0hUVktDQ1FkdzFQcUowUy1jYVpERGpDV3lHT1hoRGZ4UDBKMDRDM1UyMWsyS3d5Uy02YkJoLThGVkFaU2p6R2lrWC1aT1ozNVJ5a0xGOFl5cU80LVIxdDhweEFuTkxiTkpDUjNlelROMkxFY3dVZmpZYzZvaGdJZThLaUJiVU9fSjNUQXIwWG9mdGtDZlZPMHd1ZkZyNTlCWUYwUWl3MnkyWng4RmRqclVYMHlncVJ3a1M4U1Y5SXZmVUpiTGo1b09LMjFreVZmeGhTaEkyU1FnaWQ4clhfSzlrM2RNTnVrYTRDTUFyT0dNMVl0cHVNOXE1ZjFWcDNsd1NicEQ3RW8taTRMZjJrbkNxSlF6Z0daR1p5MmNDUlVxMV82d1EiLCJlIjoiQVFBQiJ9LCJibG9ja1BheWxvYWQiOnsidGl0bGVUcmFuc2ZlckJsb2NrSGFzaCI6IjQ4MjI5ZDgzNTc4NTRiZjY0NWJiMzZlMDY5MTM3YTA2MTM4ZWRmN2Q3ZDgzODBjMzI4Y2ZjNTI2MmEwMWU5YWMifX0","signatures":[{"protected":"eyJhbGciOiJSUzI1NiJ9","signature":"WDWvB31FMWmF9r-bR7sOU3J7SEbs_tawSgqs4sNnAmoHhfsar8tur8gJtKn0-n_aAqehvgOQ1FJDAteiZdyElJTOXncyGzmUFzZCSKwnI3gksHE4i4EdEPZrDFqB374VgaDtAkoupCjDJRZ2J6d7E5Lb3WGoK8WvXgT6vZff7cKAjkl60cW7TqEDQ5H2w6T0Vsdv_u7oMXhT6Z5uAddlBpsU7MJK1mBo13IIRHbH47uiJHqU_pl3n5zV7QnTPKx9l7iIBfVc7H4jUYH3sTRt2T39jHNE_ZevLIOcGnrLYMZ7Lg1PTWS6h3moVL4l_eIVmA3PzDltYJMComEbRhWDoA"}]}),
-        new PossessionTransferBlock(
-            {"payload":"eyJ0cmFuc2ZlcmVlIjp7Imt0eSI6IlJTQSIsIm4iOiJwMkxJVlB1dzFfdlVnaElvT0RaR0NNZThVWXB0QUFYcTdicE81bERLTUlTUl9UZ01aaC1INjVOQU9uTUJlOEVZbldwQi1yYXRJTHU4WGVORVR4Z1JSUjFCUlVmWE5pTXFDMXhtQXgycEdsb0pJb25DUmJDX1Jnc3V5VGg5cHRMZUdPbVFQMWh0LTctZjRzWk93TE5YR2U1U0xWdjBFY1QzWEJDbkhNWEJxTVZEZmpEODA1NmQ0YXlHOE9XeUQwMURMUVNFUEhYeWt4LXQ1Wk1rN1lSdEV3NE9UYzFrVFRaSThlOGZtSEZKZWlpVmJsR1cxWlZKbUUyTUtxTXk0VVpIMkVBN3lHRUJPYUpVZEMwNjlTdTZub0YyV3MxaDBtVC0zNHZhNmhRU1JPeXp2MWxnZGF5MC13dnItN01xLVJ4bEwzRXE2M1RpNEMtVEoxRk1vUnQ5clEiLCJlIjoiQVFBQiJ9LCJibG9ja1BheWxvYWQiOnsidGl0bGVUcmFuc2ZlckJsb2NrSGFzaCI6IjQ4MjI5ZDgzNTc4NTRiZjY0NWJiMzZlMDY5MTM3YTA2MTM4ZWRmN2Q3ZDgzODBjMzI4Y2ZjNTI2MmEwMWU5YWMiLCJwcmV2aW91c1JlZ2lzdHJ5VVJMIjoibG9jYWxob3N0Ojg0NDMvYXBpL3YxL3RyYW5zcG9ydC1kb2N1bWVudC10cmFuc2ZlcnMvZTkyYzI4NjM5YTc0OWJhNzE3YTdjMmRhMjgxYzhkZGEwOGRiZTAxMjEwNDdmOTE0MmVjZWMyN2QyMzRiMWFkZCJ9fQ","signatures":[{"protected":"eyJhbGciOiJSUzI1NiJ9","signature":"VmFKZ72I1bJcW-hRx7S0M8Me2ap8dmeKZoxOD3V7yvD3E0b2VWe166E7FqqeLkP43SMMy9DV5w3jykMi3BAxZWBKDtCN2HGvjnBNCU3OzSyQDaSKti8lTbNQA-IIvtW6Yk8ywGbA-EzxLxnwgespAhZYIDB2AFTfvROBWqU2Hlv8a2Xan_AiMECUYWKt7p3U4OWS7P85UgXey7BiqEjL3FyzPsGn0Kr-yFZ99xaKlK95j3JTm-P58TwhOTl6rUUMnxn9vRWZ-QWXCMwpnfK82UiSw6ocNOLuxMBDY19Ns1gKdfePjpWloD5xjfzHsJy4h0Bd4p0o_3ZPQ5shHHk8vw"}]})
-        ]
-        const thumbprintChain = await chainCrawler.chainToThumbprints(possessionChain);
-        expect(thumbprintChain[0]).toEqual("19315844aec2782bb18b");
-        expect(thumbprintChain[1]).toEqual("a162bc5f6402209fa797");
-    });
+  it("should properly convert a possession chain to thumbprints", async function() {
+    let chainCrawler = new ChainCrawler();
+    let possessionChain = [ new PossessionTransferBlock(
+      {"payload":"eyJ0cmFuc2ZlcmVlIjp7Imt0eSI6IlJTQSIsIm4iOiJ4cEZzaXhpZHgxMnUzc0I2WnBRWW44SWN1SVlqQmZHTTBSbHUzVG9vWkhPa0E1SGJwdlJ2c2FqX0hUVktDQ1FkdzFQcUowUy1jYVpERGpDV3lHT1hoRGZ4UDBKMDRDM1UyMWsyS3d5Uy02YkJoLThGVkFaU2p6R2lrWC1aT1ozNVJ5a0xGOFl5cU80LVIxdDhweEFuTkxiTkpDUjNlelROMkxFY3dVZmpZYzZvaGdJZThLaUJiVU9fSjNUQXIwWG9mdGtDZlZPMHd1ZkZyNTlCWUYwUWl3MnkyWng4RmRqclVYMHlncVJ3a1M4U1Y5SXZmVUpiTGo1b09LMjFreVZmeGhTaEkyU1FnaWQ4clhfSzlrM2RNTnVrYTRDTUFyT0dNMVl0cHVNOXE1ZjFWcDNsd1NicEQ3RW8taTRMZjJrbkNxSlF6Z0daR1p5MmNDUlVxMV82d1EiLCJlIjoiQVFBQiJ9LCJibG9ja1BheWxvYWQiOnsidGl0bGVUcmFuc2ZlckJsb2NrSGFzaCI6IjQ4MjI5ZDgzNTc4NTRiZjY0NWJiMzZlMDY5MTM3YTA2MTM4ZWRmN2Q3ZDgzODBjMzI4Y2ZjNTI2MmEwMWU5YWMifX0","signatures":[{"protected":"eyJhbGciOiJSUzI1NiJ9","signature":"WDWvB31FMWmF9r-bR7sOU3J7SEbs_tawSgqs4sNnAmoHhfsar8tur8gJtKn0-n_aAqehvgOQ1FJDAteiZdyElJTOXncyGzmUFzZCSKwnI3gksHE4i4EdEPZrDFqB374VgaDtAkoupCjDJRZ2J6d7E5Lb3WGoK8WvXgT6vZff7cKAjkl60cW7TqEDQ5H2w6T0Vsdv_u7oMXhT6Z5uAddlBpsU7MJK1mBo13IIRHbH47uiJHqU_pl3n5zV7QnTPKx9l7iIBfVc7H4jUYH3sTRt2T39jHNE_ZevLIOcGnrLYMZ7Lg1PTWS6h3moVL4l_eIVmA3PzDltYJMComEbRhWDoA"}]}),
+    new PossessionTransferBlock(
+      {"payload":"eyJ0cmFuc2ZlcmVlIjp7Imt0eSI6IlJTQSIsIm4iOiJwMkxJVlB1dzFfdlVnaElvT0RaR0NNZThVWXB0QUFYcTdicE81bERLTUlTUl9UZ01aaC1INjVOQU9uTUJlOEVZbldwQi1yYXRJTHU4WGVORVR4Z1JSUjFCUlVmWE5pTXFDMXhtQXgycEdsb0pJb25DUmJDX1Jnc3V5VGg5cHRMZUdPbVFQMWh0LTctZjRzWk93TE5YR2U1U0xWdjBFY1QzWEJDbkhNWEJxTVZEZmpEODA1NmQ0YXlHOE9XeUQwMURMUVNFUEhYeWt4LXQ1Wk1rN1lSdEV3NE9UYzFrVFRaSThlOGZtSEZKZWlpVmJsR1cxWlZKbUUyTUtxTXk0VVpIMkVBN3lHRUJPYUpVZEMwNjlTdTZub0YyV3MxaDBtVC0zNHZhNmhRU1JPeXp2MWxnZGF5MC13dnItN01xLVJ4bEwzRXE2M1RpNEMtVEoxRk1vUnQ5clEiLCJlIjoiQVFBQiJ9LCJibG9ja1BheWxvYWQiOnsidGl0bGVUcmFuc2ZlckJsb2NrSGFzaCI6IjQ4MjI5ZDgzNTc4NTRiZjY0NWJiMzZlMDY5MTM3YTA2MTM4ZWRmN2Q3ZDgzODBjMzI4Y2ZjNTI2MmEwMWU5YWMiLCJwcmV2aW91c1JlZ2lzdHJ5VVJMIjoibG9jYWxob3N0Ojg0NDMvYXBpL3YxL3RyYW5zcG9ydC1kb2N1bWVudC10cmFuc2ZlcnMvZTkyYzI4NjM5YTc0OWJhNzE3YTdjMmRhMjgxYzhkZGEwOGRiZTAxMjEwNDdmOTE0MmVjZWMyN2QyMzRiMWFkZCJ9fQ","signatures":[{"protected":"eyJhbGciOiJSUzI1NiJ9","signature":"VmFKZ72I1bJcW-hRx7S0M8Me2ap8dmeKZoxOD3V7yvD3E0b2VWe166E7FqqeLkP43SMMy9DV5w3jykMi3BAxZWBKDtCN2HGvjnBNCU3OzSyQDaSKti8lTbNQA-IIvtW6Yk8ywGbA-EzxLxnwgespAhZYIDB2AFTfvROBWqU2Hlv8a2Xan_AiMECUYWKt7p3U4OWS7P85UgXey7BiqEjL3FyzPsGn0Kr-yFZ99xaKlK95j3JTm-P58TwhOTl6rUUMnxn9vRWZ-QWXCMwpnfK82UiSw6ocNOLuxMBDY19Ns1gKdfePjpWloD5xjfzHsJy4h0Bd4p0o_3ZPQ5shHHk8vw"}]})
+    ]
+    const thumbprintChain = await chainCrawler.chainToThumbprints(possessionChain);
+    expect(thumbprintChain[0]).toEqual("19315844aec2782bb18b");
+    expect(thumbprintChain[1]).toEqual("a162bc5f6402209fa797");
+  });
 
-    it("should properly trace back the testchain", async function() {
-        const chainStart = "https://localhost:8443/spec/testchains/001/10098707e7a92f3fb7b09baeb9adb7eb3d5693447aea59581f4a835ecccb4580"
-        let chainCrawler = new ChainCrawler();
-        const chains = await chainCrawler.crawlPossessionChain(chainStart);
-        expect(chains.possessionChain.length).toEqual(3);
-        expect(chains.statuses[0]).toEqual("SURR");
-    });
+  it("should properly trace back the testchain", async function() {
+    const chainStart = "https://localhost:8443/spec/testchains/001/10098707e7a92f3fb7b09baeb9adb7eb3d5693447aea59581f4a835ecccb4580"
+    let chainCrawler = new ChainCrawler();
+    const chains = await chainCrawler.crawlPossessionChain(chainStart);
+    expect(chains.possessionChain.length).toEqual(3);
+    expect(chains.statuses[0]).toEqual("SURR");
+  });
 });
